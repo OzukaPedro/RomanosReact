45c149ae4b9ddba3edc1f3f5464bb5acfad5843b
Pedro Ozuka <pedrocampagnoli28@gmail.com>
2024-04-09 20:05:31 -0300

    facul dia 2

--- src/components/Conversao/index.jsx
+++ src/components/Conversao/index.jsx
@@ -4,13 +4,85 @@ import { StyledConverterParaRomano } from "./style";
 
 const Convert = (props) => {
   const [number, setNumber] = useState(0);
+  const [roman, setRoman] = useState('');
   const [visible, setVisible] = useState(false);
-  const [newValue, setNewValue] = useState(0);
+  const [newValue, setNewValue] = useState('0');
+
+  const divisaoCompleta = (divisor, dividendo) => {
+    return Math.floor(divisor / dividendo);
+  }
 
   const receberValor = (valor) => {
     const value = valor.target.value;
     setNumber(value);
   };
+  const converterRomano = (numer) => {
+    let romano = [];
+      if(numer > 3999){
+        alert('Numero invalido, digite um numero de 1 a 3999!');
+      }
+      if(numer % 1000 >= 1){
+        for(let i = 0; i < divisaoCompleta(numer, 1000); i++){
+          romano.push('M');
+          numer -= 1000;
+        }
+      }
+      if(numer % 900 >= 1){
+        romano.push('CM');
+          numer -= 900;
+      }
+      if(numer % 500 >= 1){
+        romano.push('D');
+          numer -= 500;
+      }
+      if(numer % 400 >= 1){
+        romano.push('CD');
+        numer -= 400;
+      }
+      if(numer % 100 >= 1){
+        for(let i = 0; i < divisaoCompleta(numer, 100); i++){
+          romano.push('C');
+          numer -= 100;
+        }
+      }
+      if(numer % 90 >= 1){
+        romano.push('XC');
+        numer -= 90;
+      }
+      if(numer % 50 >= 1){
+        romano.push('L');
+        numer -= 50;
+      }
+      if(numer % 40 >= 1){
+        romano.push('XL');
+        numer -= 40;
+      }
+      if(numer % 10 >= 1){
+        for(let i = 0; i < divisaoCompleta(numer, 10); i++){
+          romano.push('X');
+        numer -= 10;
+      }
+    }
+      if(numer % 9 >= 1){
+        romano.push('IX');
+        numer -= 9;
+      }
+      if(numer % 5 >= 5){
+        romano.push('V');
+        numer -= 5;
+      }
+      if(numer % 4 >= 4){
+        romano.push('IV');
+        numer -= 4;
+      }
+      if(numer % 1 >= 1){
+        for(let i = 0; i < divisaoCompleta(numer, 10); i++){
+          romano.push('I');
+          numer -= 1;
+        }
+      }
+      return(romano.join(""));
+  }
   const clearInput = () => {
     setNumber('');
   }
@@ -30,7 +102,7 @@ const Convert = (props) => {
             <button
               className="button"
               onClick={(props) => {
-                setNewValue(number);
+                setNewValue(converterRomano(number));
                 setVisible(true);
                 clearInput();
               }}
